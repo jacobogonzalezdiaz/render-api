8580e55d9b21d6aa592894c8d081caa132a46020
jacobogonzalezdiaz <jacobo_gonzalez@msn.com>
2023-10-16 23:06:09 +0100

    postgres añadido

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import {config} from 'dotenv';
+import pg from 'pg';
+
+config()
+
 const app = express()
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
+
 app.get('/', (req, res) => {
     res.send('Hola chachos¡¡')
 })
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
+})
+
+
 app.listen(3000)
 console.log('Server listening on port',3000)
